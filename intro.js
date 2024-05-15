273735c1875539dda433b534ed4a13746b5bf05c
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-05-15 18:13:38 +0530

    write some functions

---
+++ intro.js
@@ -0,0 +1,6 @@
+console.log("Win deasel");
+var name1 = "sameer";
+var addition = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(addition(2, 3));
---
+++ intro.ts
@@ -0,0 +1,11 @@
+
+let name:string = "sameer"
+const addition = (num1: number, num2: number): number => {
+    return num1 + num2
+}
+
+console.log(name)
+console.log(addition(2, 3.24))
+export {}
+
+let number1 = 200
