efe7146feedc4293849754b9275b012cc71a9bd1
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-05-15 18:50:53 +0530

    add funtion

---
+++ funtion.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+var touppercase = function (val) {
+    return val.toUpperCase();
+};
+console.log(touppercase("saddy"));
---
+++ funtion.ts
@@ -0,0 +1,23 @@
+const add = (num1:number , num2:number ) =>{
+    return num1 + num2;
+}
+
+const touppercase = (val : string ) =>{
+    return val.toUpperCase()
+}
+
+console.log( touppercase("saddy") )
+
+const newFunction = (name:string , email:string , isLogin:boolean=false  ) =>{
+    
+
+
+}
+
+const myArr = ["sa" , "Sa" , "SAme"]
+myArr.map((val:string):string=>{
+return val;
+})
+
+newFunction("sameer" , "same@gmail.com" )
+export {}
